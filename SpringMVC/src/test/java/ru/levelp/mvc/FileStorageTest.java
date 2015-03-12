50c07f5104fe4bf8876cc5916f81c67ecfe11a02
Denis Stepulenok <super.denis@gmail.com>
2015-03-13 00:37:57 +0300

    Сохранение / загрузка из файла

--- SpringMVC/src/test/java/ru/levelp/mvc/FileStorageTest.java
+++ SpringMVC/src/test/java/ru/levelp/mvc/FileStorageTest.java
@@ -1,13 +1,11 @@
 package ru.levelp.mvc;
 
 import org.junit.Test;
-import ru.levelp.mvc.storage.FileStorage;
 
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
-import static ru.levelp.mvc.storage.FileStorage.NOT_FOUND;
-import static ru.levelp.mvc.storage.FileStorage.search;
+import static ru.levelp.mvc.storage.FileStorage.*;
 
 /**
  * Тестирование работы файлового хранилища
@@ -17,8 +15,12 @@ public class FileStorageTest {
     @Test
     public void testFileStorage() throws IOException {
         assertEquals(NOT_FOUND, search("Нет такой строки"));
-        FileStorage.addLine("Одна строка");
-        assertEquals("Одна строка", search("строка"));
-        assertEquals("Одна строка", search("ОДНА СТРОКА"));
+        String s1 = "Одна строка";
+        addLine(s1);
+        assertEquals(s1, search("строка"));
+        assertEquals(s1, search("ОДНА СТРОКА"));
+        String s2 = "Другое резюме";
+        addLine(s2);
+        assertEquals(s2, search("РЕЗЮМЕ"));
     }
 }
