3d6db239a6975f469bad389d4b00bb1739d0256d
AleksSPb <paraleksi@gmail.com>
2015-06-16 22:24:50 +0300

    несколько "характерных" запросов для работы с этой БД Добавить пользователя Добавить задачу Получить список задач Пометить задачу как выполненную

--- SpringMVC/src/test/java/jdbc/PostgresJDBC.java
+++ SpringMVC/src/test/java/jdbc/PostgresJDBC.java
@@ -2,12 +2,14 @@ package jdbc;
 
 import org.postgresql.Driver;
 
+import java.security.MessageDigest;
 import java.sql.*;
 
 /**
  * Работа с PostgreSQL через JDBC
  */
 public class PostgresJDBC {
+    static Statement query;
     public static void main(String[] args) throws Exception {
         /*Enumeration<Driver> drivers = DriverManager.getDrivers();
         while (drivers.hasMoreElements()) {
@@ -23,13 +25,13 @@ public class PostgresJDBC {
         Driver driver = new org.postgresql.Driver();
         Class.forName("org.postgresql.Driver");
 
-        DriverManager.getDriver("jdbc:postgresql://localhost:5433/test");
+        DriverManager.getDriver("jdbc:postgresql://localhost:5432/test");
         Connection con = DriverManager.getConnection(
-                "jdbc:postgresql://localhost:5433/test",
+                "jdbc:postgresql://localhost:5432/test",
                 "postgres", "123");
 
         // Создаём запрос
-        Statement query = con.createStatement();
+        query = con.createStatement();
 
         // Набор результатов
         ResultSet resultSet =
@@ -56,10 +58,77 @@ public class PostgresJDBC {
             }
         }
         res2.close();
+        int aleksId = insertUser("AleksSPb", "myPassword", "Aleksander", "Parkhomenko");
+        int denisId = insertUser("DenisSPb", "denisPassword", "Denis", "Stepulenok");
+
+        int t1 = insertTask("Insert task", "Make query to add task to tasklist", aleksId);
+        int t2 = insertTask("Check execution", "Check how student realized add task to tasklist", denisId);
+
+        query.executeUpdate("update tasks set processed = false where id=" + t1);
+        printTaskList();
+
+
+        query.executeUpdate("delete from tasks where id>0");
+        query.executeUpdate("delete from users where login='DenisSPb'");
+
+
+
 
         //query.executeUpdate("UPDATE contact SET ... WHERE ...");
         //query.executeUpdate("DELETE FROM contact WHERE ...");
         //query.executeUpdate("INSERT INTO contact VALUES(...)")
         //query.execute("CREATE TABLE ...");
+    }
+
+    //добавить пользователя
+    static int insertUser(String userName, String pw, String name, String surname) throws Exception {
+        byte[] bytesOfMessage = pw.getBytes("UTF-8");
+
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        byte[] thedigest = md.digest(bytesOfMessage);
+
+        ResultSet resQ = query.executeQuery("SELECT id FROM users where login='" + userName + "'");
+
+        if (resQ.next()) {
+            System.out.println("Login " + userName + " present!!!");
+            return resQ.getInt("id");
+        }
+        resQ.close();
+        String insStr = "insert into users (login, hashpw, name, surname,is_active) VALUES('" + userName + "', '" + thedigest.toString() + "', '" + name + "', '" + surname + "', true)";
+        query.executeUpdate(insStr, Statement.RETURN_GENERATED_KEYS);
+        ResultSet rs = query.getGeneratedKeys();
+        if (rs.next()) return rs.getInt("id");
+        return 0;
+    }
+
+    //insert task
+    static int insertTask(String title, String descr, int executant_id) throws Exception {
+        ResultSet resQ = query.executeQuery("SELECT id FROM users where id=" + executant_id);
+
+        if (!resQ.next()) {
+            System.out.println("User id " + executant_id + " not present!!!");
+            return 0;
+        }
+        resQ.close();
+        String insStr = "insert into tasks (title, description, executant_id, processed) VALUES('" + title + "', '" + descr + "', " + executant_id + ", true)";
+        query.executeUpdate(insStr, Statement.RETURN_GENERATED_KEYS);
+        ResultSet rs = query.getGeneratedKeys();
+        if (rs.next()) return rs.getInt(1);
+
+        return 0;
+    }
+
+    //get alltask
+    static void printTaskList() throws Exception {
+        ResultSet res = query.executeQuery("SELECT tasks.id, users.login as Executant, title, description, processed from tasks LEFT JOIN users ON tasks.executant_id=users.id");
+        System.out.println("Tasks list:");
+        ResultSetMetaData rsmd = res.getMetaData();
+        while (res.next()) {
+            for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                System.out.println(rsmd.getColumnName(i) + " = " + res.getString(i));
+            }
+        }
+        res.close();
+
     }
 }
